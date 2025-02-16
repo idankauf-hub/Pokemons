6ac8b50f81d053749479182f973274704d14d731
Idan Kaufman <idankauf@gmail.com>
2025-02-16 21:40:08 +0200

    added env to from for deploy

--- front/src/constants.ts
+++ front/src/constants.ts
@@ -1,4 +1,6 @@
-export const BASE_API_URL = "http://localhost:5050/api/";
+export const BASE_API_URL =
+  import.meta.env.VITE_BASE_API_URL || "http://localhost:5050/api/";
+
 export const MAX_POKEMONS = 150;
 export const POKEMON_TAG = "Pokemon";
 export const LIMIT = 10;
