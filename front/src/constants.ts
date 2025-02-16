1e22801619a7bafca6db91b457dce644d97343fd
Idan Kaufman <idankauf@gmail.com>
2025-02-16 20:05:09 +0200

    fix caching

--- front/src/constants.ts
+++ front/src/constants.ts
@@ -1,3 +1,9 @@
 export const BASE_API_URL = "http://localhost:5050/api/";
 export const MAX_POKEMONS = 150;
-export const POKEMON_TAG = "Pokemon"
+export const POKEMON_TAG = "Pokemon";
+
+export enum RTKCacheTags {
+  Pokemons = POKEMON_TAG,
+}
+
+export const allRTKCacheTags = Object.values(RTKCacheTags);
--- front/src/services/pokemonApi.ts
+++ front/src/services/pokemonApi.ts
@@ -1,30 +1,25 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import {
+  allRTKCacheTags,
+  BASE_API_URL,
+  RTKCacheTags
+} from "../constants";
+import {
   GetPokemonsArg,
   PokemonDetails,
   PokemonListResponse,
 } from "../types/Pokemon";
-import { BASE_API_URL, POKEMON_TAG } from "../constants";
 
 export const pokemonApi = createApi({
   reducerPath: "pokemonApi",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_API_URL,
   }),
-  tagTypes: [POKEMON_TAG],
+  tagTypes: allRTKCacheTags,
   endpoints: (builder) => ({
     getPokemons: builder.query<PokemonListResponse, GetPokemonsArg>({
       query: ({ limit, offset }) => `pokemons/?limit=${limit}&offset=${offset}`,
-      providesTags: (result) =>
-        result
-          ? [
-              ...result.results.map(({ name }) => ({
-                type: "Pokemon" as const,
-                id: name,
-              })),
-              { type: POKEMON_TAG, id: "LIST" },
-            ]
-          : [{ type: POKEMON_TAG, id: "LIST" }],
+      providesTags: [RTKCacheTags.Pokemons],
     }),
     getPokemonDetails: builder.query<PokemonDetails, string>({
       query: (name) => `pokemons/${name}`,
@@ -35,18 +30,14 @@ export const pokemonApi = createApi({
         method: "POST",
         body: { name },
       }),
-      invalidatesTags: (_result, _error, arg) => [
-        { type: POKEMON_TAG, id: arg.name },
-      ],
+      invalidatesTags: [RTKCacheTags.Pokemons],
     }),
     removeFavorite: builder.mutation<void, { name: string }>({
       query: ({ name }) => ({
         url: `pokemons/${name}`,
         method: "DELETE",
       }),
-      invalidatesTags: (_result, _error, arg) => [
-        { type: POKEMON_TAG, id: arg.name },
-      ],
+      invalidatesTags: [RTKCacheTags.Pokemons],
     }),
   }),
 });
